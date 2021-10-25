19503002e6c799494b2e7bf307730dd9d8770a4a
Ferran <ferran@digipremium.net>
2021-10-25 15:39:17 +0200

    Section 12 - Lesson 68

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -3,8 +3,11 @@
 <x-form.field>
     <x-form.label name="{{ $name }}" />
 
-    <input class="border border-gray-200 p-2 w-full rounded" name="{{ $name }}" id="{{ $name }}"
-        value="{{ old($name) }}" required {{ $attributes }}>
+    <input class="border border-gray-200 p-2 w-full rounded" 
+        name="{{ $name }}" 
+        id="{{ $name }}" 
+        {{ $attributes(['value' => old($name)]) }}
+    >
 
     <x-form.error name="{{ $name }}" />
 </x-form.field>
