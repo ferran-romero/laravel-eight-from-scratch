821aed45c617472654c242ff018440fd6e5718ca
Ferran <ferran@digipremium.net>
2021-10-25 13:09:00 +0200

    Section 12 - Lesson 66

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,10 +1,10 @@
 @props(['name'])
 
-<div class="mb-6">
+<x-form.field>
     <x-form.label name="{{ $name }}" />
 
-    <textarea class="border border-gray-400 p-2 w-full" name="{{ $name }}" id="{{ $name }}"
-        required>{{ old($name) }}</textarea>
+    <textarea class="border border-gray-200 p-2 w-full rounded" name="{{ $name }}" id="{{ $name }}"
+        required {{ $attributes }}>{{ old($name) }}</textarea>
 
     <x-form.error name="{{ $name }}" />
-</div>
+</x-form.field>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,21 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-<script src="//unpkg.com/alpinejs" defer></script>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
 <style>
     html {
         scroll-behavior: smooth;
+    }
+
+    .clamp {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+
+    .clamp.one-line {
+        -webkit-line-clamp: 1;
     }
 </style>
 
@@ -23,16 +33,22 @@
 
             <div class="mt-8 md:mt-0 flex items-center">
                 @auth
-                    <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</span>
+                    <x-dropdown>
+                        <x-slot name="trigger">
+                            <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</button>
+                        </x-slot>
 
-                    <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
-                        @csrf
+                        <x-dropdown-item href="/admin/dashboard">Dashboard</x-dropdown-item>
+                        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+                        <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
 
-                        <button type="submit">Log Out</button>
-                    </form>
+                        <form id="logout-form" method="POST" action="/logout" class="hidden">
+                            @csrf
+                        </form>
+                    </x-dropdown>
                 @else
-                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                    <a href="/login" class="ml-8 mr-2 text-xs font-bold uppercase">Log in</a>
+                    <a href="/register" class="text-xs font-bold uppercase {{ request()->is('register') ? 'text-blue-500' : '' }}">Register</a>
+                    <a href="/login" class="ml-6 text-xs font-bold uppercase {{ request()->is('login') ? 'text-blue-500' : '' }}">Log In</a>
                 @endauth
 
                 <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
@@ -60,13 +76,12 @@
                             </label>
 
                             <div>
-                                <input 
-                                    id="email" 
-                                    name="email" 
-                                    type="email" 
-                                    placeholder="Your email address"
-                                    class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
-                                    
+                                <input id="email"
+                                       name="email"
+                                       type="text"
+                                       placeholder="Your email address"
+                                       class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+
                                 @error('email')
                                     <span class="text-xs text-red-500">{{ $message }}</span>
                                 @enderror
