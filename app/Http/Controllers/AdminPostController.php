7612d5c38c1d88847f973df57089c1e36e17474b
Ferran <ferran@digipremium.net>
2021-10-25 17:46:02 +0200

    Fix lesson 68: there's no published_at field in the form, get out from the validation.

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class AdminPostController extends Controller
@@ -16,59 +15,56 @@ class AdminPostController extends Controller
     }
 
     public function create()
-	{
-		return view('admin.posts.create');
-	}
+    {
+        return view('admin.posts.create');
+    }
 
-	public function store()
-	{
-		$attributes = request()->validate([
-			'title'       => 'required',
-			'thumbnail'   => ['required', 'image'],
-			'slug'        => ['required', Rule::unique('posts', 'slug')],
-			'excerpt'     => 'required',
-			'body'        => 'required',
-			'category_id' => ['required', Rule::exists('categories', 'id')],
-		]);
+    public function store()
+    {
+        Post::create(array_merge($this->validatePost(), [
+            'user_id' => request()->user()->id,
+            'thumbnail' => request()->file('thumbnail')->store('thumbnails')
+        ]));
 
-		$attributes['user_id'] = auth()->id();
-		$attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
-
-		Post::create($attributes);
-
-		return redirect('/');
-	}
+        return redirect('/');
+    }
 
     public function edit(Post $post)
     {
-        return view('admin.posts.edit', ['post' => $post]);   
+        return view('admin.posts.edit', ['post' => $post]);
     }
 
     public function update(Post $post)
     {
-        $attributes = request()->validate([
-			'title'       => 'required',
-			'thumbnail'   => ['image'],
-			'slug'        => ['required', Rule::unique('posts', 'slug')->ignore($post->id)],
-			'excerpt'     => 'required',
-			'body'        => 'required',
-			'category_id' => ['required', Rule::exists('categories', 'id')],
-		]);
+        $attributes = $this->validatePost($post);
 
-        if (isset($attributes['thumbnail'])) {
+        if ($attributes['thumbnail'] ?? false) {
             $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
         }
 
         $post->update($attributes);
 
-        return back()->with('success', 'Post updated!');
-
+        return back()->with('success', 'Post Updated!');
     }
 
     public function destroy(Post $post)
     {
         $post->delete();
-        
-        return back()->with('success', 'Post deleted!');
+
+        return back()->with('success', 'Post Deleted!');
+    }
+
+    protected function validatePost(Post $post = null): array
+    {
+        $post = $post ? $post : new Post();
+
+        return request()->validate([
+            'title' => 'required',
+            'thumbnail' => $post->exists ? ['image'] : ['required', 'image'],
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post->id)],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')],
+        ]);
     }
 }
